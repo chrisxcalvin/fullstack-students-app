5bc2b6131391c0967e4271bd87991f164c82c6ce
chrisxcalvin <chriscorda65@gmail.com>
2025-12-11 09:48:45 +0530

    message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,11 @@ import StudentList from "./components/StudentList";
 export default function App() {
   const [students, setStudents] = useState([]);
 
+  // Load API URL from .env (Netlify / Vite)
+  const API = import.meta.env.VITE_API_URL;
+
   const fetchStudents = async () => {
-    const res = await fetch("http://localhost:5000/students");
+    const res = await fetch(`${API}/students`);
     const data = await res.json();
     setStudents(data);
   };
